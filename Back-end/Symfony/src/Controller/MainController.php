62707dce587e045ab0849b897eda889e55d2d9da
Nicolas Halberstadt <halberstadtnicolas@gmail.com>
2020-03-02 14:32:23 +0000

    connexion API

--- Back-end/Symfony/src/Controller/MainController.php
+++ Back-end/Symfony/src/Controller/MainController.php
@@ -15,8 +15,27 @@ class MainController extends AbstractController
      */
     public function home()
     {
+        $client = HttpClient::create();
+
+        // Paramètres de la recherche
+        $tv_id = 1399;
+        $apiKey = "273116a48e6155286bb1ce0b34262df3";
+        $language = "fr-FR";
+        $url = "https://api.themoviedb.org/3/tv/$tv_id?api_key=$apiKey&language=$language";
+      
+        $response = $client->request("GET", $url);
+
+        // Liste des éléments retournés par l'API
+        $listElement = $response->toArray();
+        $name = $listElement["name"];
+        $plot = $listElement["overview"];
+        $networks = $listElement["networks"];
+        dump($listElement);
+
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'MainController',
+            'plot' => $plot,
+            'networks' => $networks,
+            'name' => $name
         ]);
     }
 }
