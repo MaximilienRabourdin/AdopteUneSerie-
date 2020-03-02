2242dbafadd758a157ffa8e8f9f8635b85a8297a
Your Name <you@example.com>
2020-03-02 15:10:53 +0100

    Premiere serie affiché en utilisant l'API sur twig

--- Back-end/Symfony/src/Controller/MainController.php
+++ Back-end/Symfony/src/Controller/MainController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/api")
@@ -15,8 +16,61 @@ class MainController extends AbstractController
      */
     public function home()
     {
+        $client = HttpClient::create();
+
+        // Paramètres de la recherche
+        $tv_id = 1399;
+        $apiKey = "273116a48e6155286bb1ce0b34262df3";
+        $language = "fr-FR";
+        $url = "https://api.themoviedb.org/3/tv/$tv_id?api_key=$apiKey&language=$language";
+      
+        $response = $client->request("GET", $url);
+
+        // Liste des éléments retournés par l'API
+        $listElement = $response->toArray();
+        $name = $listElement["name"];
+        $plot = $listElement["overview"];
+        $networks = $listElement["networks"];
+        dump($listElement);
+
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'MainController',
+            'plot' => $plot,
+            'networks' => $networks,
+            'name' => $name
         ]);
     }
 }
+
+
+
+
+/*protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $client = HttpClient::create();
+
+        $zipCode = "44300";
+        $countryCode = "fr";
+        $units = "metric";
+        $apiKey = "385ea34e907bdb7512e8da7b2400fc7b";
+
+        $url = "http://api.openweathermap.org/data/2.5/weather?zip=$zipCode,$countryCode&units=$units&appid=$apiKey";
+
+        $response = $client->request("GET", $url);
+
+        $forecastArray = $response->toArray();
+
+        $temperature = $forecastArray['main']['temp'];
+        $windVelocity = $forecastArray['wind']['speed'];
+
+        // ajouter en BDD un Forecast ou un Meteo
+        $forecast = new Forecast();
+        $forecast->setTemperature($temperature);
+        $forecast->setWindVelocity($windVelocity);
+        $forecast->setUpdatedAt(new \DateTime());
+
+        $this->manager->persist($forecast);
+        $this->manager->flush();
+
+        return 1;
+    }
+    */
