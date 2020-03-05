dacfe47b08532960a4f002d539777a00155cfa40
Live session user <mint@etudiant.oclock.io>
2020-03-05 10:50:44 +0100

    json maj

--- Front-end/React/src/components/App/index.js
+++ Front-end/React/src/components/App/index.js
@@ -3,21 +3,15 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 
 // == Import
-<<<<<<< HEAD
 import Header from 'src/components/Header';
-=======
 import Footer from 'src/components/Footer';
->>>>>>> Component1
 import AppStyled from './AppStyled';
 
 // == Composant
 const App = () => (
   <AppStyled>
-<<<<<<< HEAD
     <Header />
-=======
     <Footer />
->>>>>>> Component1
   </AppStyled>
 );
 
