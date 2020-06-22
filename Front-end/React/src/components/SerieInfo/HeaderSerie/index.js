c521691864a6da119e14da6a077000d08faba289
Live session user <mint@etudiant.oclock.io>
2020-06-22 15:23:56 +0200

    headerseriestyled

--- Front-end/React/src/components/SerieInfo/HeaderSerie/index.js
+++ Front-end/React/src/components/SerieInfo/HeaderSerie/index.js
@@ -2,12 +2,11 @@
 // Import : npm
 import React from 'react';
 // import { Rating } from 'semantic-ui-react'
-import MediaQuery from 'react-responsive';
 import { Divider, Icon, Table } from 'semantic-ui-react';
 import { HeartOutlined, HeartFilled } from '@ant-design/icons';
 
 // Import : local
-import './styles.css';
+import HeaderSerieStyled from './HeaderSerieStyled';
 
 // Composant
 const HeaderSerie = ({
@@ -42,47 +41,47 @@ const HeaderSerie = ({
   };
 
   return (
-    <header className="presentation">
-      <MediaQuery maxWidth={425}>
+    <HeaderSerieStyled>
+      <header>
         <img
           src={`https://image.tmdb.org/t/p/w500${image}`}
           alt="serie"
-          className="presentation-image-mobile"
+          className="presentation-image"
         />
-        <div className="presentation-content-mobile">
+        <div className="presentation-content">
           {/* Série déjà présente dans les favoris */}
           {(status === 200)
-          && (
-          <h1 className="presentation-title-mobile">{name}
-            <HeartFilled
-              onClick={handleClickDelete}
-              style={{
-                transform: 'scale(0.7)',
-                color: 'red',
-              }}
-            />
-          </h1>
-          )}
+            && (
+            <h1 className="presentation-title">{name}
+              <HeartFilled
+                onClick={handleClickDelete}
+                style={{
+                  transform: 'scale(0.7)',
+                  color: 'red',
+                }}
+              />
+            </h1>
+            )}
           {/* Série non présente dans les favoris */}
           {(status === 204)
-          && (
-          <h1 className="presentation-title-mobile">{name}
-            <HeartOutlined
-              onClick={handleClickAdd}
-              style={{
-                transform: 'scale(0.7)',
-                color: 'red',
-              }}
-            />
-          </h1>
-          )}
+            && (
+            <h1 className="presentation-title">{name}
+              <HeartOutlined
+                onClick={handleClickAdd}
+                style={{
+                  transform: 'scale(0.7)',
+                  color: 'red',
+                }}
+              />
+            </h1>
+            )}
           {(!status)
-          && <h1 className="presentation-title-mobile">{name}</h1>}
+            && <h1 className="presentation-title">{name}</h1>}
 
           <p><Icon color="yellow" name="star" /> {vote}/10 ({voteCount})</p>
           { /*  <Rating maxRating={10} defaultRating={vote} icon='star' size='small' /> */}
         </div>
-        <div className="synopsis-mobile">
+        <div className="synopsis">
           <h3>Synopsis</h3>
           <p>{overview}</p>
 
@@ -90,29 +89,29 @@ const HeaderSerie = ({
 
           <h3>Détails</h3>
           {(airDate.length > 0)
-          && <p><span style={{ fontWeight: 'bold' }}>Date de première diffusion :</span> {airDate} </p>}
+            && <p><span style={{ fontWeight: 'bold' }}>Date de première diffusion :</span> {airDate} </p>}
           {(numberSeasons.length > 0)
-         && <p><span style={{ fontWeight: 'bold' }}>Nombre de saisons :</span> {numberSeasons} </p>}
+          && <p><span style={{ fontWeight: 'bold' }}>Nombre de saisons :</span> {numberSeasons} </p>}
           {(numberEpisodes.length > 0)
-          && <p><span style={{ fontWeight: 'bold' }}>Nombre d'épisodes :</span> {numberEpisodes} </p>}
+            && <p><span style={{ fontWeight: 'bold' }}>Nombre d'épisodes :</span> {numberEpisodes} </p>}
           {(runTime.length > 0)
-          && <p><span style={{ fontWeight: 'bold' }}>Durée moyenne d'un épisode :</span> {runTime.slice(0, 2)} min</p>}
+            && <p><span style={{ fontWeight: 'bold' }}>Durée moyenne d'un épisode :</span> {runTime.slice(0, 2)} min</p>}
           {(networks.length > 0)
-          && (
-          <p><span style={{ fontWeight: 'bold' }}>Disponible sur : </span>
-            <span>
-              {networks.map((network) => <li key={network.name}>{network.name}</li>)}
-            </span>
-          </p>
-          )}
+            && (
+            <p><span style={{ fontWeight: 'bold' }}>Disponible sur : </span>
+              <span>
+                {networks.map((network) => <li key={network.name}>{network.name}</li>)}
+              </span>
+            </p>
+            )}
           {(productions.length > 0)
-          && (
-          <p><span style={{ fontWeight: 'bold' }}>Produit par :</span>
-            <span>
-              {productions.map((production) => <li key={production.name}>{production.name}</li>)}
-            </span>
-          </p>
-          )}
+            && (
+            <p><span style={{ fontWeight: 'bold' }}>Produit par :</span>
+              <span>
+                {productions.map((production) => <li key={production.name}>{production.name}</li>)}
+              </span>
+            </p>
+            )}
 
           <Divider section />
 
@@ -152,7 +151,7 @@ const HeaderSerie = ({
           >
             <Table.Body>
               {cast.map(((infos) => (
-                <Table.Row key={infos}>
+                <Table.Row key={infos.protagonist}>
                   <Table.Cell singleLine>{infos.protagonist}</Table.Cell>
                   {infos.actors.map((actor) => <Table.Cell singleLine>{actor.name}</Table.Cell>)}
                 </Table.Row>
@@ -161,113 +160,8 @@ const HeaderSerie = ({
             </Table.Body>
           </Table>
         </div>
-      </MediaQuery>
-      <MediaQuery minWidth={426}>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${image}`}
-          alt="serie"
-          className="presentation-image-desktop"
-        />
-        <div className="presentation-content-desktop">
-          {/* Série déjà présente dans les favoris */}
-          {(status === 200)
-          && (
-          <h1 className="presentation-title-desktop">{name}
-            <HeartFilled
-              onClick={handleClickDelete}
-              style={{
-                transform: 'scale(0.7)',
-                color: 'red',
-              }}
-            />
-          </h1>
-          )}
-          {/* Série non présente dans les favoris */}
-          {(status === 204)
-          && (
-          <h1 className="presentation-title-desktop">{name}
-            <HeartOutlined
-              onClick={handleClickAdd}
-              style={{
-                transform: 'scale(0.7)',
-                color: 'red',
-              }}
-            />
-          </h1>
-          )}
-          {(!status)
-          && <h1 className="presentation-title-desktop">{name}</h1>}
-
-          <p><Icon color="yellow" name="star" /> {vote}/10 ({voteCount})</p>
-          { /* <Rating maxRating={10} defaultRating={vote} icon='star' size='huge' /> */}
-        </div>
-        <div className="synopsis-desktop">
-          <h1>Synopsis</h1>
-          <p>{overview}</p>
-
-          <Divider section />
-
-          <h1>Détails</h1>
-          {(airDate.length > 0)
-            && <p><span style={{ fontWeight: 'bold' }}>Date de première diffusion :</span> {airDate} </p>}
-          {(numberSeasons.length > 0)
-           && <p><span style={{ fontWeight: 'bold' }}>Nombre de saisons :</span> {numberSeasons} </p>}
-          {(numberEpisodes.length > 0)
-            && <p><span style={{ fontWeight: 'bold' }}>Nombre d'épisodes :</span> {numberEpisodes} </p>}
-          {(runTime.length > 0)
-            && <p><span style={{ fontWeight: 'bold' }}>Durée moyenne d'un épisode :</span> {runTime.slice(0, 2)} min</p>}
-          {(networks.length > 0)
-            && (
-            <p><span style={{ fontWeight: 'bold' }}>Disponible sur : </span>
-              <span>
-                {networks.map((network) => <li key={network.name}>{network.name}</li>)}
-              </span>
-            </p>
-            )}
-          {(productions.length > 0)
-            && (
-            <p><span style={{ fontWeight: 'bold' }}>Produit par :</span>
-              <span>
-                {productions.map((production) => <li key={production.name}>{production.name}</li>)}
-              </span>
-            </p>
-            )}
-
-          <Divider section />
-
-          <h1>Saisons</h1>
-          <Table celled padded style={{ backgroundColor: '#191D1F', color: 'white' }}>
-            <Table.Body>
-              {seasons.map((season) => {
-                if (parseInt(season.season_number) > 0) {
-                  return (
-                    <Table.Row key={season.season_number}>
-                      <Table.Cell singleLine>Saison {season.season_number}</Table.Cell>
-                      <Table.Cell singleLine>{season.episode_count} épisodes</Table.Cell>
-                    </Table.Row>
-                  );
-                }
-              })}
-            </Table.Body>
-          </Table>
-
-          <Divider section />
-
-          <h1>Casting</h1>
-          <Table celled padded style={{ backgroundColor: '#191D1F', color: 'white' }}>
-            <Table.Body>
-              {cast.map(((infos) => (
-                <Table.Row key={infos}>
-                  <Table.Cell singleLine>{infos.protagonist}</Table.Cell>
-                  {infos.actors.map((actor) => <Table.Cell singleLine>{actor.name}</Table.Cell>)}
-                </Table.Row>
-              )
-              ))}
-            </Table.Body>
-          </Table>
-        </div>
-      </MediaQuery>
-    </header>
+      </header>
+    </HeaderSerieStyled>
   );
 };
 
